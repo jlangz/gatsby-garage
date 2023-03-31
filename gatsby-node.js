5c21cd60a17ed0fb89ddeac01fe10c49601e4958
Jakob Langseth <jakob.langseth@gmail.com>
2023-03-31 00:34:44 -0700

    Adding 2.6 - Adding pages to Gatsby based on pages stored in WP backend

---
+++ gatsby-node.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+exports.createPages = async ({actions, graphql}) => {
+    const pageTemplate = path.resolve("src/templates/page.js");
+    const {createPage} = actions;
+    const {data} = await graphql(`
+    query AllPagesQuery {
+        allWpPage {
+          nodes {
+            databaseId
+            blocks
+            uri
+          }
+        }
+      }
+    `);
+
+    for(let i = 0; i < data.allWpPage.nodes.length; i++){
+        const page = data.allWpPage.nodes[i];
+        createPage({
+            path: page.uri,
+            component: pageTemplate,
+        })
+    }
+}
---
+++ src/templates/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Page = () => {
+    return(
+        <div>
+            This is a page template
+        </div>
+    );
+};
+
+export default Page;
