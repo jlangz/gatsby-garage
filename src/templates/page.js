08cb64ca9f6b6c839665cce4f0dac28f3f78a247
Jakob Langseth <jakob.langseth@gmail.com>
2023-04-01 08:09:54 -0700

    3.11 - Using gatsby image and slight refactoring of page template, and reformatting with 2 space indentation

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,11 +1,56 @@
-import * as React from "react";
-import { BlockRendererProvider } from "@webdeveducation/wp-block-tools";
+import React from "react";
+import {
+  BlockRendererProvider,
+  BlockRenderer,
+  getStyles,
+  getClasses,
+} from "@webdeveducation/wp-block-tools";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 const Page = (props) => {
-    console.log("Page props:", props);
-    return(
-        <BlockRendererProvider allBlocks={props.pageContext.blocks} />
-    );
+  console.log("Page props:", props);
+  return (
+    <div>
+      <BlockRendererProvider
+        allBlocks={props.pageContext.blocks}
+        renderComponent={(block) => {
+          switch (block.name) {
+            case "core/media-text":
+              console.log("RENDER BLOCKS: ", block);
+              const content = (
+                <div
+                  className={`flex p-4 ${
+                    block.attributes.verticalAlignment === "center"
+                      ? "items-center"
+                      : ""
+                  }`}
+                >
+                  <div>
+                    <BlockRenderer blocks={block.innerBlocks} />
+                  </div>
+                </div>
+              );
+              return (
+                <div
+                  key={block.id}
+                  style={getStyles(block)}
+                  className={getClasses(block)}
+                >
+                  {block.attributes.mediaPosition === "right" && content}
+                  <div>
+                    <GatsbyImage
+                      image={block.attributes.gatsbyImage}
+                      alt="Ok"
+                    />
+                  </div>
+                  {block.attributes.mediaPosition !== "right" && content}
+                </div>
+              );
+          }
+        }}
+      />
+    </div>
+  );
 };
 
 export default Page;
