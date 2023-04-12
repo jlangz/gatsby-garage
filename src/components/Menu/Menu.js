b2043d43d4395a5a5c2339baead2acf8d3345c82
Jakob Langseth <jakob.langseth@gmail.com>
2023-04-11 22:13:35 -0700

    Formatting

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,56 +1,62 @@
-import { Link, graphql, useStaticQuery } from 'gatsby';
-import React from 'react';
+import { Link, graphql, useStaticQuery } from "gatsby";
+import React from "react";
 
 export const Menu = () => {
   const data = useStaticQuery(graphql`
-  query MainMenuQuery {
-    wp {
-      acfOptionsMainMenu {
-        mainMenu {
-          callToAction {
-            destination {
-              ... on WpPage {
-                uri
-              }
-            }
-          }
-          menuItems {
-            root {
+    query MainMenuQuery {
+      wp {
+        acfOptionsMainMenu {
+          mainMenu {
+            callToAction {
               destination {
                 ... on WpPage {
                   uri
                 }
               }
-              label
             }
-            subMenuItems {
-              destination {
-                ... on WpPage {
-                  uri
+            menuItems {
+              root {
+                destination {
+                  ... on WpPage {
+                    uri
+                  }
                 }
+                label
               }
-              label
+              subMenuItems {
+                destination {
+                  ... on WpPage {
+                    uri
+                  }
+                }
+                label
+              }
             }
           }
         }
       }
     }
-  }
   `);
   console.log("Main Menu Data: ", data);
-  const {menuItems} = data.wp.acfOptionsMainMenu.mainMenu
+  const { menuItems } = data.wp.acfOptionsMainMenu.mainMenu;
   return (
-    <div className="bg-emerald-900 text-white px-4 font-bold sticky top-0 z-20 h-16 flex justify-between">
+    <div className="sticky top-0 z-20 flex h-16 justify-between bg-emerald-900 px-4 font-bold text-white">
       <div>Logo</div>
-      <div className='flex h-full'>
+      <div className="flex h-full">
         {(menuItems || []).map((menuItem, index) => (
-          <div key={index} className='flex h-full cursor-pointer hover:bg-emerald-800'>
-            <Link to={menuItem.root.destination.uri} className='px-4 flex h-full items-center text-white no-underline'>
+          <div
+            key={index}
+            className="flex h-full cursor-pointer hover:bg-emerald-800"
+          >
+            <Link
+              to={menuItem.root.destination.uri}
+              className="flex h-full items-center px-4 text-white no-underline"
+            >
               {menuItem.root.label}
             </Link>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
